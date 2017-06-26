22859c81381906a69ec41f58ed12f17014c22618
Toke Jepsen <tokejepsen@gmail.com>
2017-06-26 13:59:56 +0200

    Print executable as well.

--- environment.py
+++ environment.py
@@ -1,1 +1,6 @@
-print "You have now cloned the conda-git-example repository, and executed the \"environment.py\" file within."
+import sys
+
+print(
+    "You have now cloned the conda-git-example repository, and executed the "
+    "\"environment.py\" file with {0}".format(sys.executable)
+)
